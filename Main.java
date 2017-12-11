2bf82d305b60b7298e8ea0c42c149a7ed0e41007
chenhonggangNZ <34049775+chenhonggangNZ@users.noreply.github.com>
2017-12-11 18:33:48 +0800

    赋给
    dfg

---
+++ Main.java
@@ -0,0 +1,13 @@
+package test;
+
+import com.lanou3g.main.Student;
+
+public class Main {
+	public static void main(String[] args) {
+		Student s = new Student();
+		s.getName();
+		System.out.println(s.getName());
+		System.out.println();
+		
+	}
+}
